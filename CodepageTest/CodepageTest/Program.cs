27bc1ad7d0e696665946fa8dfc1c2c6b654b1bb1
Will Pearson <pearswj@gmail.com>
2015-06-21 20:23:32 +0100

    initial commit

---
+++ CodepageTest/CodepageTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+
+namespace CodepageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = File.OpenRead(args[0]);
+            ZipFile zf = new ZipFile(fs); // we are getting the error on this line that 1 is not a supported code page. Parameter name:   codepage        }
+        }
+    }
+}
